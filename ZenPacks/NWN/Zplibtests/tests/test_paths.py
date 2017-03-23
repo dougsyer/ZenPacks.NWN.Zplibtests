aada7406b3a7750c6aae599cb3f5c834882bf863
dougsyer <dsyer@nwnit.com>
2017-03-23 16:22:15 -0400

    first commit

---
+++ ZenPacks/NWN/Zplibtests/tests/test_paths.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# pylint: disable=C0111, W0201
+import Globals
+from Products.ZenUtils.Utils import unused
+
+from Products.ZenTestCase.BaseTestCase import BaseTestCase
+
+# zenpacklib Imports
+from ZenPacks.zenoss.ZenPackLib.tests.ZPLTestHarness import ZPLTestHarness
+
+from test_utils import my_dev_factory
+
+unused(Globals)
+
+with open('../zenpack.yaml', 'r') as f:
+    zpl_yaml = f.read()
+
+print 'loading yaml'
+t_zp = ZPLTestHarness(zpl_yaml)
+
+
+class TestPaths(BaseTestCase):
+    """
+    test to make sure that components with nested base classes get appropriate paths so 
+    that subcomponent panels on non-containing relations work
+    """
+
+    def afterSetUp(self):
+        super(TestPaths, self).afterSetUp()
+        self.dmd.REQUEST = None
+
+        # Create standard objects the ZenPack relies on.
+        self.CFG = t_zp.cfg
+
+        # to zccess zpl config in pdb uncomment
+        # import pdb; pdb.set_trace()
+
+        _ = my_dev_factory(self.dmd)
+        self.test_my_dev = self.dmd.Devices.MyTestOrganizer.getDevices()[0]
+
+    def test_mydev_creation(self):
+        """ test basic instantiation of mydev """
+        from ZenPacks.NWN.Zplibtests.MyDev import MyDev
+
+        self.assertIsInstance(self.test_my_dev, MyDev, msg='My device not being created')
+        print self.test_my_dev.subClass1s()[0].getAllPaths()
+        print self.test_my_dev.subClass2s()[0].getAllPaths()
+        print self.test_my_dev.subClass3s()[0].getAllPaths()
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestPaths))
+    return suite
+
+if __name__ == "__main__":
+    from zope.testrunner.runner import Runner  # pylint: disable=C0413,E0611
+    runner = Runner(found_suites=[test_suite()])
+    runner.run()
---
+++ ZenPacks/NWN/Zplibtests/tests/test_utils.py
@@ -0,0 +1,97 @@
+"""
+shared utilities used for zenpack testing
+many of these utilities are copied from Zenoss's testing code in zenpacklib or vsphere tests
+"""
+# pylint: disable=C0111, W0201, W0612
+
+import logging
+
+import Globals
+
+import functools
+import random
+import importlib
+import unittest
+
+import zope.component
+from zope.traversing.adapters import DefaultTraversable
+
+from Products.Five import zcml
+import Products.ZenTestCase
+from Products.ZenTestCase.BaseTestCase import BaseTestCase, ZenossTestCaseLayer
+from Products.DataCollector.ApplyDataMap import ApplyDataMap
+from Products.ZenRelations.RelationshipBase import RelationshipBase
+from Products.ZenRelations.ToManyContRelationship import ToManyContRelationship
+from Products.ZenUtils.Utils import unused
+
+from transaction._transaction import Transaction
+
+import os
+unused(Globals)
+
+LOG = logging.getLogger('zen.nwnzplibtest')
+
+def addContained(obj, relname, target):
+    """
+    COPIED FROM zenpacklib.tests.test_exta_paths
+
+    When a manually-created python object is first added to its container, we
+    need to reload it, as its in-memory representation is changed.
+    """
+    rel = getattr(obj, relname)
+
+    # contained via a relationship
+    if isinstance(rel, ToManyContRelationship):
+        rel._setObject(target.id, target)
+        return rel._getOb(target.id)
+
+    elif isinstance(rel, RelationshipBase):
+        rel.addRelation(target)
+        return rel._getOb(target.id)
+
+    # contained via a property
+    else:
+        # note: in this scenario, you must have the target object's ID the same
+        #       as the relationship from the parent object.
+
+        assert relname == target.id
+        obj._setObject(target.id, target)
+        return getattr(obj, relname)
+
+
+def addNonContained(obj, relname, target):
+    """ add a non containnig relation """
+    rel = getattr(obj, relname)
+    rel.addRelation(target)
+    return target
+
+
+def my_dev_factory(dmd):  # pylint: disable=R0914
+    """ create a mydevice objects"""
+
+    # DeviceClass
+    dc = dmd.Devices.createOrganizer('/MyTestOrganizer')
+    dc.setZenProperty('zPythonClass', 'ZenPacks.NWN.Zplibtests.MyDev')
+
+    # cluster
+    my_dev = dc.createInstance('test_mydev')
+    # not needed just for superstition
+    my_dev.setManageIp('44.44.44.44')
+
+    # create device
+    from ZenPacks.NWN.Zplibtests.MyDev import MyDev
+    from ZenPacks.NWN.Zplibtests.SubClass1 import SubClass1
+    from ZenPacks.NWN.Zplibtests.SubClass2 import SubClass2
+    from ZenPacks.NWN.Zplibtests.SubClass3 import SubClass3
+
+    # wish the containing_relname method was a classproperty..
+    sub1 = addContained(my_dev, 'subClass1s', SubClass1('sub1'))
+    sub2 = addContained(my_dev, 'subClass2s', SubClass2('sub2'))
+    sub3 = addContained(my_dev, 'subClass3s', SubClass3('sub3'))
+
+    # use facet name to create 2nd param to do..?
+    addNonContained(sub1, "subClass2s", sub2)
+    addNonContained(sub2, "subClass3s", sub3)
+
+    return my_dev
+    
